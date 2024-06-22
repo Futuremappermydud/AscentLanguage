54b0eb1b6093c963b4172e9ae49648d2f0334f99
Futuremappermydud <54294576+Futuremappermydud@users.noreply.github.com>
2024-06-22 18:14:38 -0400

    Add More Tests

---
+++ AscentLanguage.Tests/ConditionalTests.cs
@@ -0,0 +1,48 @@
+﻿namespace AscentLanguage.Tests
+{
+	[TestClass]
+	public class ConditionalTests
+	{
+		[TestMethod]
+		public void LessThanFalse()
+		{
+			var result = AscentEvaluator.Evaluate("10 < 5");
+			Assert.AreEqual(0f, result, float.Epsilon);
+		}
+
+		[TestMethod]
+		public void LessThanTrue()
+		{
+			var result = AscentEvaluator.Evaluate("5 < 10");
+			Assert.AreEqual(1f, result, float.Epsilon);
+		}
+
+		[TestMethod]
+		public void GreaterThanFalse()
+		{
+			var result = AscentEvaluator.Evaluate("5 > 10");
+			Assert.AreEqual(0f, result, float.Epsilon);
+		}
+
+		[TestMethod]
+		public void GreaterThanTrue()
+		{
+			var result = AscentEvaluator.Evaluate("10 > 5");
+			Assert.AreEqual(1f, result, float.Epsilon);
+		}
+
+		[TestMethod]
+		public void TernaryTrue()
+		{
+			var result = AscentEvaluator.Evaluate("10 > 5 ? 7 : 8");
+			Assert.AreEqual(7f, result, float.Epsilon);
+		}
+
+		[TestMethod]
+		public void TernaryFalse()
+		{
+			var result = AscentEvaluator.Evaluate("10 < 5 ? 7 : 8");
+			Assert.AreEqual(8f, result, float.Epsilon);
+		}
+	}
+}
