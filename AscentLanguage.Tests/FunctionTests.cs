f677fa57f23acd04f18262c078fd275e51ecc9d8
Futuremappermydud <54294576+Futuremappermydud@users.noreply.github.com>
2024-06-22 18:23:23 -0400

    Fix Function Tests

--- AscentLanguage.Tests/FunctionTests.cs
+++ AscentLanguage.Tests/FunctionTests.cs
@@ -7,87 +7,87 @@ namespace AscentLanguage.Tests
 		public void BezierCurve()
 		{
 			var result = AscentEvaluator.Evaluate("bez_curve_x(0, 0, 1, 1, 0.5)");
-			Assert.AreEqual(0.5f, result, float.Epsilon);
+			Assert.AreEqual(0.5f, result, 0.000001);
 
 			var resultY = AscentEvaluator.Evaluate("bez_curve_y(0, 1, 1, 0, 0.5)");
-			Assert.AreEqual(0.75f, resultY, float.Epsilon);
+			Assert.AreEqual(0.75f, resultY, 0.000001);
 		}
 
 		[TestMethod]
 		public void Lerp()
 		{
 			var result = AscentEvaluator.Evaluate("lerp(15, 20, 0.5)");
-			Assert.AreEqual(17.5f, result, float.Epsilon);
+			Assert.AreEqual(17.5f, result, 0.000001);
 		}
 
 		[TestMethod]
 		public void Frac()
 		{
 			var result = AscentEvaluator.Evaluate("frac(15.654)");
-			Assert.AreEqual(0.654f, result, 0.0000005);
+			Assert.AreEqual(0.654f, result, 0.000001);
 		}
 
 		[TestMethod]
 		public void Abs()
 		{
 			var result = AscentEvaluator.Evaluate("abs(-5)");
-			Assert.AreEqual(5, result, 0.0000005);
+			Assert.AreEqual(5, result, 0.000001);
 		}
 
 		[TestMethod]
 		public void Exp()
 		{
-			var result = AscentEvaluator.Evaluate("exp(5, 2)");
-			Assert.AreEqual(25, result, 0.0000005);
+			var result = AscentEvaluator.Evaluate("exp(5)");
+			Assert.AreEqual(Math.Exp(5), result, 0.00001);
 		}
 
 		[TestMethod]
 		public void Sqrt()
 		{
 			var result = AscentEvaluator.Evaluate("sqrt(25)");
-			Assert.AreEqual(5, result, 0.0000005);
+			Assert.AreEqual(5, result, 0.000001);
 		}
 
 		[TestMethod]
 		public void Sign()
 		{
 			var result = AscentEvaluator.Evaluate("sign(-5)");
-			Assert.AreEqual(-1, result, 0.0000005);
+			Assert.AreEqual(-1, result, 0.000001);
 		}
 
 		[TestMethod]
 		public void Int()
 		{
 			var result = AscentEvaluator.Evaluate("int(5.31323481)");
-			Assert.AreEqual(5, result, 0.0000005);
+			Assert.AreEqual(5, result, 0.000001);
 		}
 
 		[TestMethod]
 		public void Clamp()
 		{
 			var result = AscentEvaluator.Evaluate("clamp(15, 2, 5)");
-			Assert.AreEqual(5, result, 0.0000005);
+			Assert.AreEqual(5, result, 0.000001);
 		}
 
 		[TestMethod]
 		public void Sin()
 		{
 			var result = AscentEvaluator.Evaluate("sin(1.5708)");
-			Assert.AreEqual(1, result, 0.0000005);
+			Assert.AreEqual(1, result, 0.000001);
 		}
 
 		[TestMethod]
 		public void Cos()
 		{
-			var result = AscentEvaluator.Evaluate("cos(1.5708)");
-			Assert.AreEqual(0, result, 0.0000005);
+			var result = AscentEvaluator.Evaluate("cos(6.28)");
+			Assert.AreEqual(Math.Cos(6.28), result, 0.000001);
 		}
 
 		[TestMethod]
 		public void Tan()
 		{
-			var result = AscentEvaluator.Evaluate("tan(1)");
-			Assert.AreEqual(1.5708, result, 0.0000005);
+			var result = AscentEvaluator.Evaluate("tan(0.785)");
+			Assert.AreEqual(Math.Tan(0.785), result, 0.000001);
 		}
 	}
 }
