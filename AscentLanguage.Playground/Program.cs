82462e6bffe47c5acaefe5281cac225d9cb8fb15
Futuremappermydud <54294576+Futuremappermydud@users.noreply.github.com>
2024-06-22 22:43:04 -0400

    Add Playground for testing new features

---
+++ AscentLanguage.Playground/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AscentLanguage.Playground
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string functionExpression = "function test(hello) { let x = 5 + hello; x; } test(4)";
+			float result = AscentEvaluator.Evaluate(functionExpression, null, true, true);
+		}
+	}
+}
