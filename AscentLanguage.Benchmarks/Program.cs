e0012cb54f224ad60639da2a18afc2b2b95f7ab7
Futuremappermydud <54294576+Futuremappermydud@users.noreply.github.com>
2024-06-24 22:17:09 -0400

    Add Benchmarks

---
+++ AscentLanguage.Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+using System.Diagnostics;
+
+namespace AscentLanguage.Benchmarks
+{
+	internal static class Program
+	{
+		static void Main(string[] args)
+		{
+			BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+		}
+	}
+}
